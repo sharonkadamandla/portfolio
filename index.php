42f5e5c3175c251eb57875ba557cbf6b4a6ac89d
sharonkadamandla <kadamandla36ve@gmail.com>
2026-01-29 14:30:24 -0500

    edited design & alignment

--- index.php
+++ index.php
@@ -41,66 +41,7 @@
               />
             </div>
         </div>
-        <img
-          src="./assets/arrow.png"
-          alt="Arrow icon"
-          class="icon arrow"
-          onclick="location.href='./#about'"
-        />
-    </section>
-
-    <section id="about">
-        <p class="section__text__p1">Get To Know More</p>
-        <h1 class="title">About Me</h1>
         
-        <div class="section-container">
-          
-          <div class="about-details-container">
-            <div class="text-container">
-              <p >               I design and build fast and reliable back-end services, crafting user-friendly front-ends.
-                Over my year and a half with Virtusa, I’ve containerized and shipped microservice APIs using Docker, and supported releases for enterprise clients. 
-                During my free time, I rebuilt my church’s website end-to-end, moving it from hard code to Wix and handling everything from structure to launch.
-                My bread and butter is Python & Spring Boot, with some side projects exploring NLP and Power BI dashboards because great products run on both scalable code and actionable insight.
-              </p>
-            </div>
-
-            <div class="about-containers">
-              <div class="details-container">
-                <img
-                  src="./assets/experience.png"
-                  alt="Experience icon"
-                  class="icon"
-                />
-                <h3>Experience (1+ Years)</h3><br />
-                <p>Associate Engineer Technology (2022-23) <br /><a href="https://www.virtusa.com">Virtusa,</a> India</p><br /> 
-                
-                <p>Internship (2022-22)<br /><a href="https://www.virtusa.com">Virtusa,</a> India</p><br />
-
-                <p>Teaching Assistant (2021-22)<br /><a href="https://smartinterviews.in">Smart Interviews,</a> India</p>
-              </div>
-              <div class="details-container">
-                <img
-                  src="./assets/education.png"
-                  alt="Education icon"
-                  class="icon"
-                />
-                <h3>Education</h3><br />
-                <p>PG Diploma in Data Analysis for Business Decision Making <br/> <em> Graduated with Honours</em> (2024-25) </p><br />
-               
-                <p>PG Diploma in AI Analysis, Design & Implementation <br /> <em>Graduated with Honours</em>  (2023-24)</p><br />
-                <p>Bachelors in Computer Science Engineering<br/>(2018-22)</p>
-                
-              </div>
-            </div>
-            
-          </div>
-        </div>
-        <img
-          src="./assets/arrow.png"
-          alt="Arrow icon"
-          class="icon arrow"
-          onclick="location.href='./#reviews'"
-        />
     </section>
 
     <section id="reviews">
@@ -156,10 +97,12 @@
     <section id="experience">
         <p class="section__text__p1">Explore My</p>
         <h1 class="title">Skills</h1>
+
         <div class="experience-details-container">
           <div class="about-containers">
             <div class="details-container">
               <h2 class="experience-sub-title">Languages / Frontend</h2>
+
               <div class="article-container">
                 <article>
                   <img
@@ -251,6 +194,7 @@
                 </article>
               </div>
             </div>
+
             <div class="details-container">
               <h2 class="experience-sub-title">FrameWorks / Tools / Technologies</h2>
               <div class="article-container">
@@ -299,6 +243,7 @@
                   </div>
                 </article>
               </div>
+              
             </div>
           </div>
         </div>

b12fc9b86781edc03b8748749c739e34704ebdcd
sharonkadamandla <kadamandla36ve@gmail.com>
2026-03-09 13:22:41 -0400

    added content to faith page

--- faithstatement.php
+++ faithstatement.php
@@ -1,8 +1,38 @@
 <?php include  'header.php'; ?>
-    <div class="about-details-container"><div class="text-container">
-              <p style="text-align: center;">
-                You are early! </br>
-                --- Under Construction ---
-              </p>
+<section id="faith-statement">
+  <p class="section__text__p1">
+  I believe Jesus Christ is Lord, and my work, character, and decisions are accountable to Him</br>
+</p>
+    <h1 class="title">
+                My Work is Accountable to God
+</h1>
+
+<div class="about-containers">
+
+<div class="details-container">
+<p>
+  I am not perfect, and I do not pretend to be. </br>
+  What I pursue instead is faithfulness! doing my work with integrity, humility, and a sincere desire to honor God.
+</p>
+</div>
+
+<div class="details-container">
+<p>Favorite Verse: </p>
+<a href="https://www.biblegateway.com/passage/?search=Colossians+3%3A23-24&version=NIV" target="_blank">Colossians 3:23-24</a>
+</br>
+<p>
+Favorite Song:</p>
+<a href="https://www.youtube.com/watch?v=-29gTlsdCZ8&list=RD-29gTlsdCZ8&start_radio=1" target="_blank">"The Lord Will Provide" by Worship Together</a>
+</div>
+
+<div class="text-container">
+<h1>Why I Build Websites for Churches and Ministries?</h1>
+<p>
+  As a committed Christian with biblical knowledge and experience working with faith-based organizations, I understand the unique needs of churches and ministries.
+   This allows me to build websites that accurately reflect their mission, communicate their message clearly, and support their outreach effectively.
+</p>
+
     </div>
+</div>
+</section>
 <?php include  'footer.php'; ?>
